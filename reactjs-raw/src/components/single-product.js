3f686020ab3ae8f3339f32937e73f03f822b1550
danganhkiiet <danganhkiiet@gmail.com>
2023-12-29 15:10:34 +0700

    bo loc

--- reactjs-raw/src/components/single-product.js
+++ reactjs-raw/src/components/single-product.js
@@ -1,36 +1,44 @@
 import { NavLink } from "react-router-dom";
 
-
-function SingleProduct() {
-	return (
-		<>
-			<div className="single-product">
-				<div className="product-img">
-					<NavLink to='/product-details'>
-						<img className="default-img" src="https://via.placeholder.com/550x750" alt="#" />
-						<img className="hover-img" src="https://via.placeholder.com/550x750" alt="#" />
-						<span className="out-of-stock">Hot</span>
-					</NavLink>
-					<div className="button-head">
-						<div className="product-action">
-							<a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye"></i><span>Quick Shop</span></a>
-							<a title="Wishlist" href="#"><i className=" ti-heart "></i><span>Add to Wishlist</span></a>
-							<a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
-						</div>
-						<div className="product-action-2">
-							<a title="Add to cart" href="#">Add to cart</a>
-						</div>
-					</div>
-				</div>
-				<div className="product-content">
-					<h3><NavLink to='/product-details'>Black Sunglass For Women</NavLink></h3>
-					<div className="product-price">
-						<span className="old">$60.00</span>
-						<span>$50.00</span>
-					</div>
-				</div>
-			</div>
-		</>
-	)
+function SingleProduct(props) {
+  return (
+    <>
+      <div className="single-product">
+        <div className="product-img">
+          <NavLink to='/product-details'>
+            {props.data && props.data.dien_thoai_id && (
+              <>
+                <img className="default-img" src={`http://localhost:8000/${props.data.dien_thoai_id.hinh_anh[0].duong_dan}`} alt="#" />
+                <img className="hover-img" src={`http://localhost:8000/${props.data.dien_thoai_id.hinh_anh[0].duong_dan}`} alt="#" />
+              </>
+            )}
+            <span className="out-of-stock">Hot</span>
+          </NavLink>
+          <div className="button-head">
+            <div className="product-action">
+              <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className="ti-eye"></i><span>Quick Shop</span></a>
+              <a title="Wishlist" href="#"><i className="ti-heart"></i><span>Add to Wishlist</span></a>
+              <a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+            </div>
+            <div className="product-action-2">
+              <a title="Add to cart" href="#">Add to cart</a>
+            </div>
+          </div>
+        </div>
+        <div className="product-content">
+		{props.data && props.data.dien_thoai_id && (
+              <>
+          <h3><NavLink to='/product-details'>{props.data.dien_thoai_id.ten}</NavLink></h3>
+          <div className="product-price">
+            <span className="old">{props.data.mau_sac_id.ten}</span>
+            <span>{props.data.gia_ban}</span>
+          </div>
+		  </>
+            )}
+        </div>
+      </div>
+    </>
+  )
 }
+
 export default SingleProduct;
