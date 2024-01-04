44f5c86850751a62bd92d790337afcd0cdf065f2
danganhkiiet <danganhkiiet@gmail.com>
2024-01-04 15:23:12 +0700

    vnpay

--- reactjs-raw/src/components/pages/cart/total-amount.js
+++ reactjs-raw/src/components/pages/cart/total-amount.js
@@ -1,9 +1,39 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useLocation  } from 'react-router-dom';
+import notifySuccess from "../../items/noti_success";
 
 export function TotalAmount(props) {
+    const location = useLocation();
+    useEffect(() => {
+        // Lấy thông tin từ URL
+        const searchParams = new URLSearchParams(location.search);
+        const vnp_ResponseCode = searchParams.get('vnp_ResponseCode');
+        const vnp_TransactionNo = searchParams.get('vnp_TransactionNo');
+
+    
+        // Xử lý logic dựa trên thông tin từ URL
+        if (vnp_ResponseCode === '00') {
+            notifySuccess('Thanh toán thành công, đơn hàng đặt thành công')
+            axios.post('http://127.0.0.1:8000/api/phieu-xuat/them-moi', {
+                khach_hang_id : user.id,
+                tong_tien : props.tongtien
+            })
+            .then((response) => {
+                setTimeout(() => {
+                    window.location.href = '/cart'; 
+                  }, 3000);
+                })
+            .catch((error) => {
+                // Xử lý lỗi nếu có
+                console.error('Lỗi khi xóa sản phẩm:', error);
+            });
+        } else {
+          // Xử lý trường hợp giao dịch không thành công
+        }
+      }, [location]);
     const [user,setUser] = useState(JSON.parse(Cookies.get('user')));
     const handlBuy = () => {
         if(props.payment === 1)
@@ -15,6 +45,20 @@ export function TotalAmount(props) {
             .then((response) => {
                 alert('đặt hàng thành công');
                 window.location.reload();
+            })
+            .catch((error) => {
+                // Xử lý lỗi nếu có
+                console.error('Lỗi khi xóa sản phẩm:', error);
+            });
+        }
+        if(props.payment === 2)
+        {
+            axios.post('http://127.0.0.1:8000/api/thanh-toan-vnpay', {
+                khach_hang_id : user.id,
+                tong_tien : props.tongtien
+            })
+            .then((response) => {
+                window.location.href = response.data.data;
             })
             .catch((error) => {
                 // Xử lý lỗi nếu có
@@ -44,7 +88,7 @@ export function TotalAmount(props) {
                                 <li className="last">Tổng tiền<span>{props.tongtien}</span></li>
                             </ul>
                             <div className="button5">
-                                <button onClick={handlBuy} className="btn"> Mua ngay </button>
+                                <button onClick={handlBuy} name="redirect" className="btn"> Mua ngay </button>
                                 <a href="#" className="btn">Continue shopping</a>
                             </div>
                         </div>
