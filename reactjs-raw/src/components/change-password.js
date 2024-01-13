bf519663d19e7bbbb8a49bc34d3504921fe9d9b6
BakaPanther <dhieu226@gmail.com>
2024-01-13 09:43:27 +0700

    Change pw

--- reactjs-raw/src/components/change-password.js
+++ reactjs-raw/src/components/change-password.js
@@ -1,12 +1,27 @@
+import { NavLink } from "react-router-dom";
 import Footer from "./footer";
 import Header from "./header";
 
 export default function ChangePassword(){
     return (
         <>
-        <Header />
+        <div className="reset-password-container">
+                <div className="reset-password form">
+                    <header>Thay đổi mật khẩu</header>
+                    <form >
+                        <input type="text" placeholder="Nhập mật khẩu cũ" name="password"/>
+                        <input type="text" placeholder="Nhập mật khẩu mới" name="re-password"/>
+                        <input type="text" placeholder="Xác nhận mật khẩu mới" name="accept-password"/>
+                        <input type="submit" className="button" value="Xác nhận" />
+                    </form>
+                    <div className="signup">
+                        <span className="signup">Không cần thay đổi mật khẩu?
+                            <label><NavLink to='/login'>Login</NavLink></label>
+                        </span>
+                    </div>
+                </div>
 
-        <Footer />
+            </div>
         </>
     )
 }
